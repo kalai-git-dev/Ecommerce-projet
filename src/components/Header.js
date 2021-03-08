61c1ba6d4f002c59248433067dc3d26159355636
kalai-git-dev <kalai.amine@hotmail.fr>
2021-03-08 16:29:31 +0100

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
 import React from "react";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
+import SearchIcon from "@material-ui/icons/Search";
 function Header() {
-  return <div>header</div>;
+  return (
+    <div className="header">
+      <div>
+        <h1>Kalai-Commerce</h1>
+        <form>
+          <input type="text" placeholder="Search..." />
+          <SearchIcon variant="contained" color="primary"></SearchIcon>
+        </form>
+      </div>
+
+      <ul className="navigation">
+        <li>Acceuil</li>
+        <li>Produits</li>
+        <li>Contact </li>
+      </ul>
+      <ShoppingCartIcon />
+    </div>
+  );
 }
 
 export default Header;
