78cba5c6003429d86e15800684711cf0045fc4b3
kalai-git-dev <kalai.amine@hotmail.fr>
2021-04-01 18:31:34 +0200

    fetch data and add carousel

---
+++ src/components/Caroussel.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Chaussure from "../assets/chaussures.jpeg";
+import Plombier from "../assets/Plombier.jpeg";
+import Vetements from "../assets/vetements.jpeg";
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Caroussel() {
+  return (
+    <Carousel fade indicators>
+      <Carousel.Item>
+        <img
+          style={{ height: 500 }}
+          className="image_carousel"
+          src={Chaussure}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>Chaussures</h3>
+          <p>
+            Consultez et déposez vos petites annonces gratuites d'achat et de
+            vente d'occasion dans la categorie chaussure.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          style={{ height: 500 }}
+          className="image_carousel"
+          src={Plombier}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Services</h3>
+          <p>Proposez les services et Trouvez des emplois</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          style={{ height: 500 }}
+          className="image_carousel"
+          src={Vetements}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Vêtements</h3>
+          <p>
+            Vêtements occasion France. Des milliers de petites annonces près de
+            chez vous
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Caroussel;
--- src/pages/Acceuil.js
+++ src/pages/Acceuil.js
@@ -1,20 +1,63 @@
 import React from "react";
 import CardOffer from "../components/CardOffer";
+import Caroussel from "../components/Caroussel";
 import { Link } from "react-router-dom";
 function Acceuil({ data, isLoading }) {
   console.log(data);
+  let filterData1;
+  let filterData2;
+  let filterData3;
+  if (!isLoading) {
+    filterData1 = data.filter((offer) => {
+      return offer.category.includes("Vêtements");
+    });
+  }
+  if (!isLoading) {
+    filterData2 = data.filter((offer) => {
+      return offer.category.includes("Chaussures");
+    });
+  }
+  if (!isLoading) {
+    filterData3 = data.filter((offer) => {
+      return offer.category.includes("Services");
+    });
+  }
   return isLoading ? (
     <p>en cours de chargemnt..</p>
   ) : (
-    <div>
-      {data.map((offer) => {
-        return (
-          <Link to={"offer/" + offer._id} key={offer._id}>
-            <CardOffer offer={offer} />
-          </Link>
-        );
-      })}
-    </div>
+    <>
+      <Caroussel />
+      <div>
+        <h2>Vêtements</h2>
+        {filterData1.map((offer) => {
+          return (
+            <Link to={"offer/" + offer._id} key={offer._id}>
+              <CardOffer offer={offer} />
+            </Link>
+          );
+        })}
+      </div>
+      <div>
+        <h2>Chaussures</h2>
+        {filterData2.map((offer) => {
+          return (
+            <Link to={"offer/" + offer._id} key={offer._id}>
+              <CardOffer offer={offer} />
+            </Link>
+          );
+        })}
+      </div>
+      <div>
+        <h2>Services</h2>
+        {filterData3.map((offer) => {
+          return (
+            <Link to={"offer/" + offer._id} key={offer._id}>
+              <CardOffer offer={offer} />
+            </Link>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
